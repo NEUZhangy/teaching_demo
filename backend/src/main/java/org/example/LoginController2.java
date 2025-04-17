f3e886c9584d30dc3725adf365008fe91481773f
Ying Zhang <yingzhang@vt.edu>
2025-04-17 12:19:04 -0400

    add comment

--- backend/src/main/java/org/example/LoginController2.java
+++ backend/src/main/java/org/example/LoginController2.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+// controller two for string based token
 
 @RestController
 public class LoginController2 {
